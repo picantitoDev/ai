9ad0c2a9a89502c59df6d3e9cfc9a3f457f2eb4d
Piero Arturo <piero.dev@outlook.com>
2025-06-25 18:20:58 -0500

    app.py

--- flask-helloworld/app.py
+++ flask-helloworld/app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return 'Hello from Flask on Render!'
